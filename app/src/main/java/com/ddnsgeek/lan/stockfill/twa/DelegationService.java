1bc5fa00b6da7e12985bfa86c0c7d8183b0262f9
beatzaplenty <wayne.bennett@live.com>
2025-11-21 17:44:42 +1000

    Buiuld APK 	modified:   .gitignore 	new file:   .gradle/8.11.1/checksums/checksums.lock 	new file:   .gradle/8.11.1/executionHistory/executionHistory.bin 	new file:   .gradle/8.11.1/executionHistory/executionHistory.lock 	new file:   .gradle/8.11.1/fileChanges/last-build.bin 	new file:   .gradle/8.11.1/fileHashes/fileHashes.bin 	new file:   .gradle/8.11.1/fileHashes/fileHashes.lock 	new file:   .gradle/8.11.1/fileHashes/resourceHashesCache.bin 	new file:   .gradle/8.11.1/gc.properties 	new file:   .gradle/buildOutputCleanup/buildOutputCleanup.lock 	new file:   .gradle/buildOutputCleanup/cache.properties 	new file:   .gradle/buildOutputCleanup/outputFiles.bin 	new file:   .gradle/vcs-1/gc.properties 	new file:   android.keystore 	new file:   app-release-bundle.aab 	new file:   app-release-signed.apk 	new file:   app-release-signed.apk.idsig 	new file:   app-release-unsigned-aligned.apk 	new file:   app/build.gradle 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/ddnsgeek/lan/stockfill/twa/Application.java 	new file:   app/src/main/java/com/ddnsgeek/lan/stockfill/twa/DelegationService.java 	new file:   app/src/main/java/com/ddnsgeek/lan/stockfill/twa/LauncherActivity.java 	new file:   app/src/main/res/drawable-anydpi/shortcut_legacy_background.xml 	new file:   app/src/main/res/drawable-hdpi/ic_notification_icon.png 	new file:   app/src/main/res/drawable-hdpi/splash.png 	new file:   app/src/main/res/drawable-mdpi/ic_notification_icon.png 	new file:   app/src/main/res/drawable-mdpi/splash.png 	new file:   app/src/main/res/drawable-xhdpi/ic_notification_icon.png 	new file:   app/src/main/res/drawable-xhdpi/splash.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_notification_icon.png 	new file:   app/src/main/res/drawable-xxhdpi/splash.png 	new file:   app/src/main/res/drawable-xxxhdpi/ic_notification_icon.png 	new file:   app/src/main/res/drawable-xxxhdpi/splash.png 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   app/src/main/res/raw/web_app_manifest.json 	new file:   app/src/main/res/values/colors.xml 	new file:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/xml/filepaths.xml 	new file:   app/src/main/res/xml/shortcuts.xml 	new file:   build.gradle 	new file:   build/reports/problems/problems-report.html 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   manifest-checksum.txt 	new file:   settings.gradle 	new file:   store_icon.png 	new file:   twa-manifest.json

---
+++ app/src/main/java/com/ddnsgeek/lan/stockfill/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.ddnsgeek.lan.stockfill.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+

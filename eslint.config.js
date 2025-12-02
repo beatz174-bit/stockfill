8a2b6a183b133a1ac8619c0ff4b705101264ce3f
beatz174-bit <beatz174@gmail.com>
2025-12-02 18:45:56 +1000

    Ignore coverage reports in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['node_modules', 'build', 'dist', 'public'],
+    ignores: ['node_modules', 'build', 'dist', 'public', 'coverage', 'coverage-reports'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
